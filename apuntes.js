28b734ade3865dac7db574b1b10fed2c6b63c8bb
SharonZC <ssofiazozoaga@gmail.com>
2024-08-07 20:51:30 -0600

    Add files via upload

---
+++ apuntes.js
@@ -0,0 +1,47 @@
+//
+
+
+console.log('hola consola');
+
+//var (era la variable de antes)
+let nombre = "Sharon"; //variable
+
+const pi = 3.14; //el valor no cambia
+
+//Tipos de datos
+
+//No definido
+let noDefinido;
+//Número
+let cantidad = 50;
+//Cadena de texto o String
+let bebida ="agua";
+//boleanos
+let activo = true;
+
+//objetos
+//Persona: nombre, apellido, edad, peso
+let persona = {nombre : 'Sharon', apellido : 'Zozoaga', edad : 18,}
+console.log(persona);
+//Arrays colección
+let personas = [  
+    {nombre : 'lily', apellido : 'garcia', edad : 20,},
+    {nombre : 'rose', apellido : 'Amedrani', edad : 19,},
+    {nombre : 'sasha', apellido : 'trusova', edad : 23,},
+    persona 
+]
+console.log(personas);
+console.log(personas[2]);
+
+//Funciones JS
+//Listado de instrucciones
+function suma(a, b) {
+    return a+b;
+}
+let resultadoSuma = suma(9,7);
+console.log(resultado);
+function multiplicacion(a, b) {
+    return a*b;
+}
+let resultado = multiplicacion(5,7);
+console.log(resultado);
